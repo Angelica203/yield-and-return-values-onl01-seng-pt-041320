6f8744ee282a396f5033c0b33b18817037fda87f
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-25 20:10:51 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -8,5 +8,25 @@ def hello(array)
   end
 end
 
-
+binding.pry
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+
+
+
+
+
+
+
+# require 'pry'
+
+# def hello(array)
+#   i = 0
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+# end
+
+# binding.pry
+# hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
